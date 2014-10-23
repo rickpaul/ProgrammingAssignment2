ab5632091b0d54e08dae5a2554d86aa89fa1be2a
rickpaul <rickpaul.dhindsa@gmail.com>
2014-10-23 16:25:16 -0400

    adding completed homework

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,54 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This is a family of functions that will solve for and cache an inverted matrix.
+## The goal of the function is to save repeated expensive computations.
 
-## Write a short comment describing this function
+## This function returns a matrix and related functions that can cache its 
+## result (an inverted matrix).
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    Inverted <- NULL
+    set <- function(y) {
+        x <<- y
+        Inverted <<- solve(x)
+    }
+    #returns the original matrix
+    get <- function() x
+    getInverted <- function() Inverted
+    solveInverted <- function() {Inverted <<- solve(x)}
+    list(set = set, get = get,
+         getInverted = getInverted,
+         solveInverted = solveInverted)
 }
 
 
-## Write a short comment describing this function
+## This function returns an inverted matrix. It first checks to see if the matrix
+## is cached. If not, it computes it. If so, it computes the matrix and replaces
+## the matrix in the cache.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    #check if the caller is passing in a new matrix. 
+    if(!missing(...))
+    {
+        # I'm a litle mad I couldn't put this in the upper (outer)
+        # if statement with an '&' between them. It insisted on checking
+        # both arguments.
+        if(identical(x$get(),...))
+        {
+            message("New matrix isn't different. Getting cached data")
+            return(x$getInverted())
+        }
+        message("Setting new matrix")
+        x$set(...)
+        return(x$getInverted())
+    }
+    else if(is.null(x$getInverted()))
+    {
+        message("Solving matrix inversion for first time.")
+        x$solveInverted()
+        return(x$getInverted())       
+    }
+    else
+    {
+        message("No matrix passed. Getting cached data")
+        return(x$getInverted())
+    }    
 }
